98853c762fa5705e798089bff1c8f98020608813
Samantha Meason <sbmeason@gmail.com>
2020-02-25 01:01:19 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,7 +8,11 @@ class Person
 
   def get_married(person)
     self.partner = person
+    if person.class != Person
+      raise PartnerError
+    else 
     person.partner = self
+    end
   end
 
 class PartnerError < StandardError
